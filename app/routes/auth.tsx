d8308341c226af06716e146d64b2ffaa8f34f590
Rajeev.v <rajeevvalechha@gmail.com>
2025-08-12 12:29:24 +0530

    Initial project commit

--- app/routes/auth.tsx
+++ app/routes/auth.tsx
@@ -1,10 +1,10 @@
-import {useEffect, useState} from "react";
 import {usePuterStore} from "~/lib/puter";
+import {useEffect} from "react";
 import {useLocation, useNavigate} from "react-router";
 
 export const meta = () => ([
-    { title: 'Resumind | Auth'},
-    { name: 'description', content: 'Log into Your Account' },
+    { title: 'Resumind | Auth' },
+    { name: 'description', content: 'Log into your account' },
 ])
 
 const Auth = () => {
@@ -14,31 +14,31 @@ const Auth = () => {
     const navigate = useNavigate();
 
     useEffect(() => {
-        if (auth.isAuthenticated) navigate(next);
+        if(auth.isAuthenticated) navigate(next);
     }, [auth.isAuthenticated, next])
 
     return (
-        <main className="bg-[url('/images/bg-main.svg')] bg-cover min-h-screen flex items-center justify-center">
+        <main className="bg-[url('/images/bg-auth.svg')] bg-cover min-h-screen flex items-center justify-center">
             <div className="gradient-border shadow-lg">
                 <section className="flex flex-col gap-8 bg-white rounded-2xl p-10">
                     <div className="flex flex-col items-center gap-2 text-center">
-                        <h1> Welcome </h1>
-                        <h2> Log In to Continue Your Job Journey </h2>
+                        <h1>Welcome</h1>
+                        <h2>Log In to Continue Your Job Journey</h2>
                     </div>
                     <div>
-                        { isLoading ? (
+                        {isLoading ? (
                             <button className="auth-button animate-pulse">
-                                <p> Signing you in... </p>
+                                <p>Signing you in...</p>
                             </button>
                         ) : (
                             <>
                                 {auth.isAuthenticated ? (
                                     <button className="auth-button" onClick={auth.signOut}>
-                                        <p> Log Out </p>
+                                        <p>Log Out</p>
                                     </button>
                                 ) : (
                                     <button className="auth-button" onClick={auth.signIn}>
-                                        <p> Log In </p>
+                                        <p>Log In</p>
                                     </button>
                                 )}
                             </>
